f241309d12d88223329e2199a8ecc802d37f27d5
yeouya <jjae1022@gmail.com>
2021-02-24 06:25:20 +0900

    refactor: Header 컴포넌트 리팩토링
    SearchForm 으로 따로 분리했던 컴포넌트를 다시 Header 컴포넌트에 병합

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,44 @@
-import { Link } from "react-router-dom";
-import SearchForm from "./SearchForm";
+import { FormEvent } from "react";
+import { useHistory, Link } from "react-router-dom";
+import { FcSearch } from "react-icons/fc";
+import { MdClose } from "react-icons/md";
+import useInput from "../hooks/useInput";
 import style from "./Header.module.css";
 
-const { header, container, heading } = style;
+const { header, container, heading, form, input, icon } = style;
 
 export default function Header() {
+  const [query, setQuery, bindQuery] = useInput();
+  const { push } = useHistory();
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!query.trim()) {
+      return;
+    }
+    push(`/search/${query}`);
+    setQuery("");
+  };
+
+  const handleClick = () => {
+    setQuery("");
+  };
+
   return (
     <header className={header}>
       <div className={container}>
         <h1 className={heading}>
-          <Link to="/">React Movie</Link>
+          <Link to="/">react movie</Link>
         </h1>
-        <SearchForm />
+        <form className={form} onSubmit={handleSubmit}>
+          <span className={icon}>
+            <FcSearch />
+          </span>
+          <input className={input} type="search" title="검색" {...bindQuery} />
+          <span className={icon} onClick={handleClick}>
+            <MdClose />
+          </span>
+        </form>
       </div>
     </header>
   );
