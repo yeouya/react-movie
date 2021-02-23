8aaac8f0a906013cba8d08e194d6fb5f4ed70944
yeouya <jjae1022@gmail.com>
2021-02-24 08:07:21 +0900

    refactor: router 컴포넌트 basename 추가

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Header />
       <Switch>
         <Route path="/" exact>
